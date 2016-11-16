acb1d56414fa95fa1b2c6d84aa9ee1d453d73989
Sam Lee <samlee405@yahoo.com>
2016-11-16 13:54:24 -0800

    Implemented sentence feature

--- app.py
+++ app.py
@@ -1,14 +1,19 @@
 from flask import Flask
 import sample
-import wordFrequency
 import os
 
 app = Flask(__name__)
 
 @app.route('/')
-
-def getSentence():
+def getWord():
     return sample.sample()
+
+@app.route('/<num>')
+def getSentence(num):
+    sentence = ""
+    for _ in range(num):
+        sentence += sample.sample() + " "
+    sentence.strip()
 
 if (__name__) == ('__main__'):
     port = int(os.environ.get("PORT", 5000))
