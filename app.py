86f37f2e5bf5b4740a660c06455ff1613eeb2cd1
Sam Lee <samlee405@yahoo.com>
2016-11-16 13:26:49 -0800

    Fixed app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask
 import sample
 import wordFrequency
+import os
 
 app = Flask(__name__)
 
@@ -10,4 +11,5 @@ def getSentence():
     return sample.sample()
 
 if (__name__) == ('__main__'):
-    app.run()
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=True, host = '0.0.0.0', port=port)
